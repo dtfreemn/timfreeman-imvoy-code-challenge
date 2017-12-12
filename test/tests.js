a71447268d8be26539b1bbe111cbdab80e41ae86
Tim Freeman <d.timothy.freeman@gmail.com>
2017-12-11 22:20:29 -0500

    more abstraction of tests to improve maintainability. moved test data to object

--- test/tests.js
+++ test/tests.js
@@ -1,27 +1,36 @@
 const tester = require('../tester.js').main.tester
 const assert = require('chai').assert
 
+let testData = {
+  one: {
+    string: "ABCDABAA",
+    total: "$32.40"
+  },
+  two: {
+    string: "CCCCCCC",
+    total: "$7.25"
+  },
+  three: {
+    string: "ABCD",
+    total: "$15.40"
+  },
 
-describe("Test one -- ABCDABAA", function() {
-  let string = "ABCDABAA"
-  let total = "$32.40"
-  it(`should return a total of $${total} when it is scanned in that order`, function() {
-    assert(total, tester(string))
+}
+
+describe(`Test one -- ${testData.one.string}`, function() {
+  it(`should return a total of ${testData.one.total} when it is scanned in that order`, function() {
+    assert(testData.one.total, tester(testData.one.string))
   })
 })
 
-describe("Test two -- CCCCCCC", function() {
-  let string = "CCCCCCC"
-  let total = "$7.25"
-  it(`should return a total of $${total} when it is scanned in that order`, function() {
-    assert(total, tester(string))
+describe(`Test two -- ${testData.two.string}`, function() {
+  it(`should return a total of ${testData.two.total} when it is scanned in that order`, function() {
+    assert(testData.two.total, tester(testData.two.string))
   })
 })
 
-describe("Test two -- ABCD", function() {
-  let string = "ABCD"
-  let total = "15.40"
-  it(`should return a total of $${total} when it is scanned in that order`, function() {
-    assert(total, tester(string))
+describe(`Test three -- ${testData.three.string}`, function() {
+  it(`should return a total of ${testData.three.total} when it is scanned in that order`, function() {
+    assert(testData.three.total, tester(testData.three.string))
   })
 })
