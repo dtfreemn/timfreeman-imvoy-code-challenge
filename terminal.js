19159df10c0aa2bacecd8442fa7259ddc6725fdf
Tim Freeman <d.timothy.freeman@gmail.com>
2017-12-11 19:12:52 -0500

    refactored total and scan functions a bit

--- terminal.js
+++ terminal.js
@@ -4,52 +4,35 @@ class Terminal {
     this.cart = {}
   }
 
-  prices() {
+  listPrices() {
     return this.prices
   }
 
   scan(item) {
-    if (!this.cart[item]) {
-      this.cart[item] = 1
-    } else {
-      this.cart[item] += 1
-    }
+    this.cart[item] ? this.cart[item] += 1 : this.cart[item] = 1
   }
 
   total() {
     let total = 0
     Object.keys(this.cart).forEach(key => {
-      if (!this.prices[key].volume) {
-        total += this.prices[key].price * this.cart[key]
+      let cartQuantity = this.cart[key]
+      let item = this.prices[key]
+      let volume = item.volume
+      
+      if (!item.volume || cartQuantity < volume.quantity) {
+        total += item.price * cartQuantity
       } else {
-        if (this.cart[key] >= this.prices[key].volume.quantity) {
-          total += (this.prices[key].volume.price * Math.floor(this.cart[key]/this.prices[key].volume.quantity)) + (this.prices[key].price * (this.cart[key] % this.prices[key].volume.quantity))
-        } else {
-          total += this.prices[key].price * this.cart[key]
-        }
+        total += (volume.price * Math.floor(cartQuantity/volume.quantity)) + (item.price * (cartQuantity % volume.quantity))
       }
     })
-    this.cart = {}
     return "$" + total.toFixed(2)
+  }
+
+  clear() {
+    this.cart = {}
   }
 }
 
 exports.main = {
   terminal: Terminal
 }
-
-
-// let priceReference = {
-//   A: {price: 2, volume: {quantity: 4, price: 7}},
-//   B: {price: 12},
-//   C: {price: 1.25, volume: {quantity: 6, price: 6}},
-//   D: {price: .15}
-// }
-
-// function tester(testString) {
-//   let splitTest = testString.split('')
-//   let t = new Terminal(priceReference)
-  
-//   splitTest.forEach(item => t.scan(item))
-//   return t.total()
-// }
